e88ff5ce39b40ebafc693c7b889f7607415957ab
Vasu Seelamneni <Vasu_Seelamneni@Dell.com>
2024-11-02 16:20:57 +0800

    MOA:{Unit test created for Genre Controller using MockMvc.}

--- src/test/java/com/msp/movie/db/moviedbservice/MovieDbServiceApplicationTests.java
+++ src/test/java/com/msp/movie/db/moviedbservice/MovieDbServiceApplicationTests.java
@@ -1,19 +1,52 @@
 package com.msp.movie.db.moviedbservice;
 
+import com.msp.movie.db.controller.GenreController;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class MovieDbServiceApplicationTests {
+
+	@Autowired
+	private GenreController genreController;
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
 	}
 
-	@GetMapping("/test")
-	public String test() {
-		return "Hello, World!";
+	@Test
+	void testGenreController() throws Exception {
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/genres/all"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.[0].name").value("Action"));
+
+		/*mockMvc.perform(MockMvcRequestBuilders.get("/api/genres/find/1"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.[0].name").value("Action"));
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/genres/save"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.[0].name").value("Thriller"));
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/genres/delete/1"))
+				.andExpect(MockMvcResultMatchers.status().isOk());*/
+
 	}
 
 }
