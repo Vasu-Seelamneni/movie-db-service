8f8915eefb552b5ca92de6b2eb53c9500570d93b
Vasu Seelamneni <Vasu_Seelamneni@Dell.com>
2024-11-02 16:21:49 +0800

    MOA:{Applied logging mechanism using logback in Genre Controller.}

--- src/main/java/com/msp/movie/db/controller/GenreController.java
+++ src/main/java/com/msp/movie/db/controller/GenreController.java
@@ -2,6 +2,8 @@ package com.msp.movie.db.controller;
 
 import com.msp.movie.db.model.Genre;
 import com.msp.movie.db.service.GenreService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +13,7 @@ import java.util.List;
 @RequestMapping("/api/genres")
 public class GenreController {
 
+    private static final Logger log = LoggerFactory.getLogger(GenreController.class);
     private final GenreService genreService;
 
     @Autowired
@@ -22,21 +25,25 @@ public class GenreController {
 
     @GetMapping("/all")
     public List<Genre> getAllGenres() {
+        log.info("Get all genres");
         return genreService.findAll();
     }
 
     @PostMapping("/save")
     public Genre saveGenre(@RequestBody Genre genre) {
+        log.info("Saved a genre");
         return genreService.save(genre);
     }
 
     @GetMapping("/find/{id}")
     public Genre findGenreById(@PathVariable Long id) {
+        log.info("Searched a genre");
         return genreService.findById(id);
     }
 
     @DeleteMapping("/delete/{id}")
     public void deleteGenre(@PathVariable Long id) {
+        log.info("Deleted a genre");
         genreService.deleteGenre(id);
     }
 }
